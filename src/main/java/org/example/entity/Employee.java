7d349ef6bd84a3f2fb4ea75a84b9e63d722eb754
Chippo156 <vovannghiahiep150623@gmail.com>
2025-03-04 16:16:34 +0700

    no pattern

---
+++ src/main/java/org/example/entity/Employee.java
@@ -0,0 +1,23 @@
+package org.example.entity;
+
+public class Employee {
+    private String name;
+    private String position;
+
+    public Employee(String name, String position) {
+        this.name = name;
+        this.position = position;
+    }
+    public void work(){
+        if ("Đội Trưởng".equals(position)) {
+            System.out.println(name + " đi tuần và gán việc nhân viên.");
+        } else if ("Giám Đốc".equals(position)) {
+            System.out.println(name + " quản lý công ty.");
+        } else if ("Nhân Viên VP".equals(position)) {
+            System.out.println(name + " pha và phê.");
+        } else {
+            System.out.println(name + " thực hiện công việc chung.");
+        }
+    }
+
+}
